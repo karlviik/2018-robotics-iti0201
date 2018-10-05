69becf05714843e604f39f38ca0245e6db6681e5
karlviik <viik.karl2@gmail.com>
2018-10-05 09:05:38 +0300

    robot v3

--- S/robot.py
+++ S/robot.py
@@ -11,6 +11,7 @@ distance_from_object = robot.get_front_middle_ir()
 robot.set_wheels_speed(30)
 while distance_from_object > 0.18:
     distance_from_object = robot.get_front_middle_ir()
+    print(distance_from_object)
     rospy.sleep(0.05)
 
 # Stop the robot when done
