a24f4278b57474ce186f20d1ea7723ccd792e372
karlviik <viik.karl2@gmail.com>
2018-10-05 08:16:16 +0300

    robot v1

---
+++ S/robot.py
@@ -0,0 +1,17 @@
+from PiBot import PiBot
+import rospy
+
+# Create a robot instance
+robot = PiBot()
+
+# Get distance from object using the front middle IR sensor
+distance_from_object = robot.get_front_middle_ir()
+
+# Drive towards object
+robot.set_wheels_speed(30)
+while distance_from_object > 0.2:
+    distance_from_object = robot.get_front_middle_ir()
+    rospy.sleep(0.05)
+
+# Stop the robot when done
+robot.set_wheels_speed(0)
