04617e15cbb9bc6fa456bbd8390be4c3bae52cca
karlviik <viik.karl2@gmail.com>
2018-10-10 22:22:48 +0300

    L1 rewrite test 4

--- L1/robot.py
+++ L1/robot.py
@@ -2,6 +2,9 @@ from PiBot import PiBot
 import rospy
 
 robot = PiBot()
+# This whole section is just to make writing stuff easier, means linel1() is same as robot.get_rightmost_toomuchtext()
+# To note is that left and right sides are switched as in this task robot always moves backwards, so might aswell
+# treat robot's back as its front.
 linel1 = robot.get_rightmost_line_sensor
 linel2 = robot.get_second_line_sensor_from_right
 linel3 = robot.get_third_line_sensor_from_right
@@ -22,62 +25,47 @@ def speedr(perc):
     robot.set_left_wheel_speed(-perc)
 
 
-left_second = robot.get_second_line_sensor_from_left()
-"""
-while True:
-    # if data from sensor says that the colour is black, we proceed moving back (-30)
-    while left_second < 300:
-        left_second = robot.get_second_line_sensor_from_left()
-        right_third = robot.get_third_line_sensor_from_right()
-        robot.set_wheels_speed(-20)
-    # if the colour is not black - stop
-    robot.set_wheels_speed(0)
-    # We turn left by default. If right_most sensor is black, we understand that we need to continue turning in
-    # opposite direction. It is the fastest way to get a black colour on left_second sensor.
-    # If the colour on left_second is black and the colour on right_most is white, start the first loop
-    while True:
-        left_second = robot.get_second_line_sensor_from_left()
-        right_most = robot.get_rightmost_line_sensor()
-        if left_second < 300 < right_most:
-            break
-        else:
-            if left_second > 300:
-                if right_most < 300:
-                    while left_second > 300:
-                        print("third")
-                        left_second = robot.get_second_line_sensor_from_left()
-                        robot.set_left_wheel_speed(-15)
-                        robot.set_right_wheel_speed(15)
-                else:
-                    robot.set_left_wheel_speed(15)
-                    robot.set_right_wheel_speed(-15)
-"""
+def turn(perc):  # negative speed turns left, positive right
+    speedr(-perc)
+    speedl(perc)
+
+
+def waitblack(side):  # waits for black to go under corresponding sensor
+    if side:
+        while liner3 > 700:
+            rospy.sleep(0.025)
+    else:
+        while linel3 > 700:
+            rospy.sleep(0.025)
+
+
 lastside = 1
 while True:
     speed(20)
     while linel3() < 300 and liner3() < 300:
         rospy.sleep(0.025)
-    if linel3() > 700 and liner3() > 700:
+    if linel3() > 700 and liner3() > 700:  # prolly better to use whiles instead of ifs to not do useless tasks
         speed(0)
         if linel2() < 300 or linel1() < 300:
-            speedl(-20)
-            speedr(20)
+            turn(-20)
             lastside = 0
+            waitblack(0)
         elif liner2() < 300 or liner1() < 300:
-            speedl(20)
-            speedr(-20)
+            turn(20)
             lastside = 1
+            waitblack(1)
         else:
             if lastside:
-                speedl(20)
-                speedr(-20)
+                turn(20)
+                waitblack(1)
             else:
-                speedl(-20)
-                speedr(20)
-    elif linel3() > 700:
+                turn(-20)
+                waitblack(0)
+    elif linel3() > 700:  # these can't really use while loops
         speedr(15)
         lastside = 1
+        rospy.sleep(0.025)
     elif liner3() > 700:
         speedl(15)
         lastside = 0
-    rospy.sleep(0.025)
+        rospy.sleep(0.025)
