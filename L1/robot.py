f33fd4acc08d5c099d2299af5226eb1347018454
karlviik <viik.karl2@gmail.com>
2018-10-10 19:51:19 +0300

    Merge branch 'master' of https://gitlab.cs.ttu.ee/kaviik/iti0201-2018
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/workspace.xml

--- L1/robot.py
+++ L1/robot.py
@@ -1,51 +1,33 @@
-import rospy
 from PiBot import PiBot
+import rospy
 
-
-def turn(degrees, side, speed, currentspeed):
-    wheelturngoal = (robot.AXIS_LENGTH / (360 / degrees) / robot.WHEEL_DIAMETER) * 360
-    multiplier = 1
-    if side == 0:
-        multiplier = -1
-    wheelturngoal = wheelturngoal * multiplier
-    lencgoal = robot.get_left_wheel_encoder() + wheelturngoal
-
-    if side == 1:
-        robot.set_left_wheel_speed(currentspeed + speed)
-        robot.set_right_wheel_speed(currentspeed - speed)
-        while lencgoal > robot.get_left_wheel_encoder():
-            rospy.sleep(0.05)
-    else:
-        robot.set_left_wheel_speed(currentspeed - speed)
-        robot.set_right_wheel_speed(currentspeed + speed)
-        while lencgoal < robot.get_left_wheel_encoder():
-            rospy.sleep(0.05)
-    robot.set_wheels_speed(currentspeed)
-
-
-# Create a robot instance
 robot = PiBot()
+left_second = robot.get_second_line_sensor_from_left()
 
-# Get distance from object using the front middle IR sensor
-distance_from_object = robot.get_front_middle_ir()
-
-# Drive towards object
-robot.set_wheels_speed(30)
-while distance_from_object > 0.18:
-    distance_from_object = robot.get_front_middle_ir()
-    print(distance_from_object)
-    rospy.sleep(0.05)
-robot.set_wheels_speed(0)
-
-turn(90, 0, 17, 0)
-
-lenc = robot.get_left_wheel_encoder()
-robot.set_wheels_speed(-30)
-distance_wall = robot.get_front_right_ir()
-while distance_wall < 80:
-    distance_wall = robot.get_front_right_ir()
-    rospy.sleep(0.05)
-robot.set_wheels_speed(0)
-
-# Stop the robot when done
-robot.set_wheels_speed(0)
+while True:
+    # if data from sensor says that the colour is black, we proceed moving back (-30)
+    while left_second < 300:
+        left_second = robot.get_second_line_sensor_from_left()
+        right_third = robot.get_third_line_sensor_from_right()
+        robot.set_wheels_speed(-20)
+    # if the colour is not black - stop
+    robot.set_wheels_speed(0)
+    # We turn left by default. If right_most sensor is black, we understand that we need to continue turning in
+    # opposite direction. It is the fastest way to get a black colour on left_second sensor.
+    # If the colour on left_second is black and the colour on right_most is white, start the first loop
+    while True:
+        left_second = robot.get_second_line_sensor_from_left()
+        right_most = robot.get_rightmost_line_sensor()
+        if left_second < 300 < right_most:
+            break
+        else:
+            if left_second > 300:
+                if right_most < 300:
+                    while left_second > 300:
+                        print("third")
+                        left_second = robot.get_second_line_sensor_from_left()
+                        robot.set_left_wheel_speed(-15)
+                        robot.set_right_wheel_speed(15)
+                else:
+                    robot.set_left_wheel_speed(15)
+                    robot.set_right_wheel_speed(-15)
