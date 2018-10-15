cbe3875c0196c8862be489454cf60b40e81486a1
karlviik <viik.karl2@gmail.com>
2018-10-15 23:23:48 +0300

    l2

--- L2/robot.py
+++ L2/robot.py
@@ -17,17 +17,6 @@ left_distance = robot.get_right_wheel_encoder
 right_distance = robot.get_right_wheel_encoder
 
 
-class Counter:
-    def __init__(self, count=0):
-        self.count = count
-
-    def add_one(self):
-        self.count += 1
-
-
-counter = Counter()
-
-
 def speed(perc):
     """
     Set wheel speeds, but treats backwards as forwards and vice versa. Mostly for shorter typing.
@@ -67,6 +56,32 @@ def turn(perc):  # negative speed turns left, positive right
     """
     speedr(-perc)
     speedl(perc)
+
+
+def preciseturn(side, speed):  # side 0 is left, side 1 is right
+    speed(0)
+    wheelturngoal = (robot.AXIS_LENGTH / robot.WHEEL_DIAMETER) * 90
+    enc = robot.get_right_wheel_encoder()
+    if side:
+        encgoal = robot.get_right_wheel_encoder() - wheelturngoal
+        turn(speed)
+        while enc > encgoal:
+            rospy.sleep(0.01)
+            enc = robot.get_right_wheel_encoder()
+    else:
+        encgoal = robot.get_right_wheel_encoder() + wheelturngoal
+        turn(-speed)
+        while enc < encgoal:
+            rospy.sleep(0.01)
+            enc = robot.get_right_wheel_encoder()
+
+
+def crossing(crosscount):
+    if crosscount % 3 == 0:
+        preciseturn(0, 20)
+    elif crosscount % 3 == 2:
+        preciseturn(1, 20)  # make the bot turn 90 degrees RIGHT
+    return crosscount + 1
 
 
 def move():
@@ -126,100 +141,31 @@ def move():
 
 
 def main():
+    countandturn = 0
     while True:
         move()
-        speed(0)
-        distance = left_distance()
-        print("start turning")
+        print(countandturn)
+        countandturn = crossing(countandturn)
+        #speed(0)
+        #distance = left_distance()
+        #print("start turning")
         # 592  - a rule of thumb. Just calculated this value and added to the formula.
         # formula is designed to turn left
-        while left_distance() < distance + (pi * 592) / 4:
-            turn(-20)
-        move()
-        move()
-        distance = right_distance()
+        #while left_distance() < distance + (pi * 592) / 4:
+        #    turn(-20)
+        #move()
+        #move()
+        #distance = right_distance()
         # to turn right
-        while right_distance() > distance - (pi * 592) / 4:
-            turn(20)
-        speed(0)
+        #while right_distance() > distance - (pi * 592) / 4:
+        #    turn(20)
+        #speed(0)
 
 
 main()
 
-"""Take a shot at following the line. White is lava.""""""
-import rospy
-from PiBot import PiBot
-from math import floor
+"""
 
-robot = PiBot()
-# This whole section is just to make writing stuff easier, means linel1() is same as robot.get_rightmost_toomuchtext()
-# To note is that left and right sides are switched as in this task robot always moves backwards, so might aswell
-# treat robot's back as its front.
-getlinel1 = robot.get_rightmost_line_sensor
-getlinel2 = robot.get_second_line_sensor_from_right
-getlinel3 = robot.get_third_line_sensor_from_right
-getliner1 = robot.get_leftmost_line_sensor
-getliner2 = robot.get_second_line_sensor_from_left
-getliner3 = robot.get_third_line_sensor_from_left
-
-
-def setspeed(perc):
-    robot.set_wheels_speed(-perc)
-
-
-def setspeedl(perc):
-    robot.set_right_wheel_speed(-perc)
-
-
-def setspeedr(perc):
-    robot.set_left_wheel_speed(-perc)
-
-
-def turn(perc):  # negative speed turns left, positive right
-    if perc > 0:
-        setspeedl(perc)
-    else:
-        setspeedr(perc)
-
-
-def turnstat(perc):  # negative speed turns left, positive right
-    setspeedr(-perc)
-    setspeedl(perc)
-
-
-def preciseturn(degrees, mode, speed):  # negative deg left, positive right. Mode 0 is both wheels, mode 1 is 1 wheel
-    setspeed(0)
-    wheelturngoal = (robot.AXIS_LENGTH / (360 / degrees) / robot.WHEEL_DIAMETER) * 360
-    if mode:
-        wheelturngoal = wheelturngoal * 2
-    if degrees > 0:
-        lencgoal = robot.get_right_wheel_encoder() - wheelturngoal
-        setspeedl(speed)
-        if not mode:
-            setspeedr(-speed)
-        clenc = robot.get_right_wheel_encoder()
-        while clenc > lencgoal:
-            rospy.sleep(0.01)
-            clenc = robot.get_right_wheel_encoder()
-    else:
-        rencgoal = robot.get_left_wheel_encoder() + wheelturngoal
-        setspeedr(speed)
-        if not mode:
-            setspeedl(-speed)
-        crenc = robot.get_left_wheel_encoder()
-        while crenc > rencgoal:
-            rospy.sleep(0.01)
-            crenc = robot.get_left_wheel_encoder()
-
-
-def crossing(crosscount):
-    if crosscount % 3 == 0:
-        preciseturn(-90, 1, 20)
-    # if crosscount % 3 == 1:
-        # make the bot ignore everything and move straight
-    elif crosscount % 3 == 2:
-        preciseturn(90, 1, 20)  # make the bot turn 90 degrees RIGHT
-    return crosscount + 1
 
 
 def updatelines():
@@ -267,4 +213,5 @@ while True:
         lastside = 0
         while getliner3() > 700 and getlinel3() < 300:
             rospy.sleep(0.01)
-    rospy.sleep(0.01)"""
+    rospy.sleep(0.01)
+"""
