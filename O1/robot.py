475f41db4c44609e2cc293021d13eb4be1e07afd
karlviik <viik.karl2@gmail.com>
2018-10-26 09:05:52 +0300

    O1

--- O1/robot.py
+++ O1/robot.py
@@ -6,6 +6,34 @@ robot = PiBot()
 # TODO: function that scans the field for the object
 # TODO: function that moves towards the object until required distance
 
+
+def set_speed(speed):
+    robot.set_left_wheel_speed(speed)
+    robot.set_right_wheel_speed(speed)
+
+
+def turn(degrees, side, speed):
+    wheelturngoal = (robot.AXIS_LENGTH / (360 / degrees) / robot.WHEEL_DIAMETER) * 360
+    multiplier = 1
+    if side == 0:
+        multiplier = -1
+    wheelturngoal = wheelturngoal * multiplier
+    lencgoal = robot.get_left_wheel_encoder() + wheelturngoal
+
+    if side == 1:
+        robot.set_left_wheel_speed(speed)
+        robot.set_right_wheel_speed(- speed)
+        while lencgoal > robot.get_left_wheel_encoder():
+            rospy.sleep(0.05)
+    else:
+        robot.set_left_wheel_speed(- speed)
+        robot.set_right_wheel_speed(speed)
+        while lencgoal < robot.get_left_wheel_encoder():
+            rospy.sleep(0.05)
+    robot.set_wheels_speed(0)
+
+
+
 def scan_for_object():
     pass
     # TODO: implement a way of finding the object, perhaps based on difference of sensors.
@@ -13,3 +41,7 @@ def scan_for_object():
     # Prolly do full circle? Or not?
     # If sensor step 1 is far, step 2 is closer, step 3 is far
     # then step 2 has object. Prolly 10cm difference is okay?
+
+turn(180, 1, 20)
+rospy.sleep(1)
+turn(90, 0, 20)
