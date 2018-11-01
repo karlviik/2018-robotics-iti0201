652ee259caa8f6e2b1ab00fb2ae1f416bf45ec79
karlviik <viik.karl2@gmail.com>
2018-11-01 22:42:12 +0200

    object

--- O1/robot.py
+++ O1/robot.py
@@ -55,6 +55,7 @@ def scan_for_object():
 
 
 def check_cache_for_object(cache):
+    print(cache)
     if cache[0] < cache[1] < cache[2] > cache[3] < cache[4]:
         return True, -1
     if cache[0] < cache[1] > cache[2] > cache[3] < cache[4]:
@@ -69,7 +70,7 @@ def check_cache_for_object(cache):
 def scan_for_object_vol2():
     print("Started scanning")
     left_encoder = robot.get_left_wheel_encoder()
-    step = (360 * robot.AXIS_LENGTH / robot.WHEEL_DIAMETER) / 40  # step of turning because some idea
+    step = (360 * robot.AXIS_LENGTH / robot.WHEEL_DIAMETER) / 20  # step of turning because some idea
     wheelturngoal = left_encoder + step  # full 360 degree turn
     turn(16, 1)  # does turning with speed 13 clockwise
     fmir = get_fmir()
