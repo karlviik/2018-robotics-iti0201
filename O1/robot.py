1449b2392b7da737b1fcf88aed8047ba8aee2c32
karlviik <viik.karl2@gmail.com>
2018-11-08 15:57:58 +0200

    O1

--- O1/robot.py
+++ O1/robot.py
@@ -124,6 +124,7 @@ def scan_for_object():
 
         # for when bot has reached end of sector
         if (abs(last_trenc - last_tlenc) - abs(lenc - renc)) > sectorcounter * degstep * 2:  # wheelturngoal < last_tlenc:
+            print((abs(last_trenc - last_tlenc) - abs(lenc - renc)), sectorcounter * degstep * 2)
             tempmeasure = total / measurecounter  # average measurement of fmir during sector
             total, measurecounter = 0, 0  # zeroes them for next sector
 
