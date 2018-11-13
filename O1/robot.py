bea8eb55840d0d9285fab956dec61e17c4edf8b4
karlviik <viik.karl2@gmail.com>
2018-11-13 12:24:04 +0200

    Stack

--- O1/robot.py
+++ O1/robot.py
@@ -18,6 +18,7 @@ def fmir_buffer_init():
     fmir_average = total / (i + 1)
     return fmir_average, [fmir_average, fmir_average, fmir_average]
 
+
 def fmir_buffering(variables):
     """
     See, if fmir value is correct. If not, return last fmir.
@@ -27,6 +28,7 @@ def fmir_buffering(variables):
     """
     buffer = variables["fmir_buffer"]
     fmir = robot.get_front_middle_ir()
+    variables["last_fmir"] = variables["front_mid_ir"]
     buffer.pop(0)
     buffer.append(fmir)
     average = (buffer[0] + buffer[1] + buffer[2]) / 3
@@ -34,9 +36,9 @@ def fmir_buffering(variables):
         variables["front_mid_ir"] = fmir
     else:
         variables["front_mid_ir"] = variables["last_fmir"]
-    variables["last_fmir"] = fmir
     variables["fmir_buffer"] = buffer
     return variables
+
 
 def sense(variables):
     variables["left_enc"] = robot.get_left_wheel_encoder()
@@ -48,21 +50,18 @@ def sense(variables):
 def plan(variables):
     if variables["phase"] == "scanning":
         pass
-    elif variables["phase"] == "turn back to object":
-        pass
-    elif variables["phase"] == "":
-        pass
-    elif variables["phase"] == "scanning":
-        pass
-    elif variables["phase"] == "scanning":
-        pass
-    elif variables["phase"] == "scanning":
-        pass
-    elif variables["phase"] == "scanning":
-        pass
-    elif variables["phase"] == "scanning":
-        pass
-
+        # if fmir difference is more than 15 cm or something
+            # then stop scanning and zero in on the object?
+        # else
+            # if haven't turned like 1.5 turns:
+                # change wheel speed according to encoders and stuff, P/I/D controlling or something
+    elif variables["phase"] == "zero to obj":
+        pass  # this part should aim itself directly at the object with PIDing
+        # take 3 measurements per angle and try to get as close as possible. When got to as close as possible, check the direction haven't checked yet
+    elif variables["phase"] == "move to obj":
+        pass  # this part should move straight to obj until like 15cm close while PIDing
+    elif variables["phase"] == "blind to obj":
+        pass  # this part should move the last X distance to obj
     return variables
 
 
