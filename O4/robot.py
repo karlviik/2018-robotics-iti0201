b017bfff4a87ad23d565d9c52ce7d4eeafc5b523
karlviik <viik.karl2@gmail.com>
2018-11-15 22:14:34 +0200

    Added some more stuff

--- O4/robot.py
+++ O4/robot.py
@@ -187,7 +187,7 @@ def plan(variables):
         # if it is being run the first time
         if variables["left_speed"] == 0:
             # calculate the amount of degrees the bot has to turn
-            variables["how_much_to_turn"] = ((variables["closest_wall_encoder_diff"] - 140) - robot.WHEEL_DIAMETER * (variables["left_enc"] - variables["right_enc"]) / (2 * robot.AXIS_LENGTH)) % 360
+            variables["how_much_to_turn"] = ((variables["closest_wall_encoder_diff"] - 155) - robot.WHEEL_DIAMETER * (variables["left_enc"] - variables["right_enc"]) / (2 * robot.AXIS_LENGTH)) % 360
             variables["how_much_has_turned"] = 0
 
             # if calculated degrees are more than 180 degrees (so over half a turn clockwise)
